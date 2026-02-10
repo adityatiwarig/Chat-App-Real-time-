2cb264f779375b321d8d50b9990e783b43949df5
adityatiwarig <aditya2461tiwari@gmail.com>
2026-02-11 00:04:30 +0530

    Added controller of register and login

---
+++ backend/controllers/userController.js
@@ -0,0 +1,87 @@
+import jwt from "jsonwebtoken";
+import { User } from "../models/userModel.js";
+import bcrypt from "bcryptjs"
+
+
+export const register = async(req,res) => {
+    try {
+        const {fullName , username, password , confirmPassword , gender} = req.body;
+        if(!fullName || !username|| !password || !confirmPassword || !gender){
+            return res.status(400).json({message:"All fields are required."})
+        }
+
+        if(password !== confirmPassword){
+            return res.status(400).json({message:"Password do not match."})
+        }
+
+        const user = User.findOne({username});
+
+        if(user){
+            return res.status(400).json({message:"Username already exists."})
+        }
+
+        const hashedPassword = await bcrypt.hash(password,10);
+        
+        //profilePhoto
+
+        const maleProfilePhoto = `https://avatar.iran.liara.run/public/boy?username=${username}`;
+        const femaleProfilePhoto = `https://avatar.iran.liara.run/public/girl?username=${username}`;
+
+        await User.create({
+            fullName,
+            username,
+            password :hashedPassword,
+            gender,
+            profilePhoto :gender == "male" ? maleProfilePhoto :femaleProfilePhoto
+        });
+        
+        return res.status(200).json({
+            message:"Account Created successfully..",
+            success:true
+        })
+    } catch (error) {
+        console.log(error);
+        
+    }
+}
+
+export const login = async(req,res) => {
+    try {
+        const {username,password} = req.body;
+        if(!username || !password) {
+            return res.status(400).json({message : "All fields are required."});
+        };
+
+        const user = await User.findOne({username});
+        if(!user) {
+            return res.status(400).json({
+                message:"Incorrect username or password.",
+                success:false
+            })
+        };
+
+        const isPasswordMatch = await bcrypt.compare(password, user.password);
+        if(!isPasswordMatch){
+            return res.status(400).json({
+                message:"Incorrect username or password.",
+                success:false
+            })
+        }
+
+        const tokenData={
+            userId : user._id
+        }
+        const token = await jwt.sign(tokenData, process.env.JWT_SECRET_KEY, {expiresIn:'1d'});
+
+        return res.status(200).cookie("token", token, {maxAge: 1*24*60*60*1000, httpsOnly:true, sameSite:'strict'}).json({
+            _id:user._id,
+            username:user.username,
+            fullName:user.fullName,
+            profilePhoto:user.profilePhoto
+        });
+
+
+    } catch (error) {
+        
+    }
+};
